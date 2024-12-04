2ddd8b76b2bd940a09bd95b340e7092bd1fd51d5
Aleksei <krivoshapkinaaa@gmail.com>
2024-12-04 19:41:35 +0300

    работа с клавиатурами

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
+from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
+                           ReplyKeyboardRemove)
+from environs import Env
 
-from config_data.config import Config, load_config
-from aiogram import Dispatcher, Bot
-from handlers import other_hanlers, user_handlers
+env = Env()
+env.read_env()
 
-async def main() -> None:
-    config: Config = load_config() #Загружаем токен
+bot = Bot(token=env('BOT_TOKEN'))
+dp = Dispatcher()
 
-    #инициализируем бота и диспетчер
-    bot = Bot(config.tg_bot.token)
-    dp = Dispatcher
+button1 = KeyboardButton(text='Кнопка 1')
+button2 = KeyboardButton(text='Кнопка 2')
+button3 = KeyboardButton(text='Кнопка 3')
+button4 = KeyboardButton(text='Кнопка 4')
+button5 = KeyboardButton(text='Кнопка 5')
+button6 = KeyboardButton(text='Кнопка 6')
+button7 = KeyboardButton(text='Кнопка 7')
+button8 = KeyboardButton(text='Кнопка 8')
+button9 = KeyboardButton(text='Кнопка 9')
 
-    #Создаём роутеры
-    dp.include_router(user_handlers.router)
-    dp.include_router(other_hanlers.router)
+keyboard1 = ReplyKeyboardMarkup(keyboard=[
+    [button1, button1],
+    [button3, button4],
+    [button5, button6],
+    [button7, button8],
+    [button9]
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+],
+resize_keyboard=True
+)
 
-asyncio.run(main())
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text='Это твоя клавиатура', reply_markup=keyboard1)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
